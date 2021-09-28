d24a5ef44e59095bdbbefbb664ee647879759faa
Chen An <v-chenan1@microsoft.com>
2021-09-28 04:50:18 +0000

    Add project files.

---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp53
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
